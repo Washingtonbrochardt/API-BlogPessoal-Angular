9290594e3458c9cb1f2aa2925ec78881e522db1a
waskhalifa <washingtonbrochardt@hotmail.com>
2021-12-02 12:52:19 -0300

    @Washingtonbrochardt [Task: CREATE routes]

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"", redirectTo: "login", pathMatch: "full"},
+
+  {path:"login", component: LoginComponent},
+  {path:"cadastrar", component: CadastrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
