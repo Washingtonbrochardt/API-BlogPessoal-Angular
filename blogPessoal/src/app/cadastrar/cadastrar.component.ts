d1e3435be9330f6ff123e9bd8de053c0b0bdb0bf
waskhalifa <washingtonbrochardt@hotmail.com>
2021-12-09 00:49:20 -0300

    @Washingtonbrochardt [Criando COMPONENT Inicio]

--- blogPessoal/src/app/cadastrar/cadastrar.component.ts
+++ blogPessoal/src/app/cadastrar/cadastrar.component.ts
@@ -34,6 +34,7 @@ export class CadastrarComponent implements OnInit {
     if(this.usuario.senha != this.confirmarSenha){
         alert('As senhas estao incorretas.')
     }else{
+      console.log(this.usuario)
       this.authService.cadastrar(this.usuario).subscribe((resp: Usuario )=> {
         this.usuario= resp
         this.router.navigate(['/login'])
--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment.prod';
 import { Usuario } from './../model/Usuario';
 import { UserLogin } from './../model/UserLogin';
 import { HttpClient } from '@angular/common/http';
@@ -12,9 +13,18 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   entrar(userLogin:UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('https://localhost:8080/usuarios/logar', userLogin)
   }
   cadastrar(usuario:Usuario): Observable<Usuario>{
-    return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)
+    return this.http.post<Usuario>('https://localhost:8080/usuarios/cadastrar', usuario)
+  }
+  logado(){
+    let ok = false
+
+    if(environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 }
